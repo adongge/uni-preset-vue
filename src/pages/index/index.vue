9322e92f1da0a19a4a00c2afec2797130a96057d
adong <adddge@aliyun.com>
2026-01-02 15:32:55 +0800

    多个小程序修改

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,90 +1,91 @@
 <template>
 	<view class="content">
+		<view class="app-header"></view>
 		<view class="text-area">
 			<text class="title">阿东的 uniapp 项目</text>
 		</view>
-    
-    <view class="version">{{ version }}</view>
-		
+    	<view class="version">{{ version }}</view>
 	</view>
 </template>
 
 <script>
-	import store from '@/store'
-	import {
-		login
-	} from '@/api/user'
-	import {
-		utils
-	} from '@/common/utils'
-	import {
-		USER_DATA_KEY,
-		USER_DATA_KEY_ID,
-		VERSION,
-		PLATFORM_ID
-	} from '@/config'
-	export default {
-		data() {
-			return {
-				title: "",
-				version: VERSION
-			}
-		},
-		onLoad() {
-			this.title = utils.getUserToken()
-			if (!this.title) {
-				this.userLogin()
-			}
-		},
-		onShow() {
-			utils.setTabbar(0, false)
-		},
-		methods: {
-			userLogin() {
-				uni.login({
-					success: res => {
-						if (res.code) {
-							login({
-								code: res.code,
-								platform_id: PLATFORM_ID
-							}).then((result) => {
-								console.log(result)
-								utils.cache(USER_DATA_KEY, result.data.token, 'd1')
-								utils.cache(USER_DATA_KEY_ID, result.data.id, 'd1')
-								this.title = result.data.token
-							}).catch((result) => {
-								console.error(result)
-								uni.showToast({
-									title: '请求登录失败',
-									icon: 'none',
-									duration: 3000
-								});
-							})
-						}
+import {
+	login
+} from '@/api/user'
+import {
+	utils
+} from '@/common/utils'
+import {
+	USER_DATA_KEY,
+	USER_DATA_KEY_ID,
+	VERSION,
+	PLATFORM_ID
+} from '@/config'
+export default {
+	data() {
+		return {
+			title: "",
+			version: VERSION
+		}
+	},
+	onLoad() {
+		this.title = utils.getUserToken()
+		if (!this.title) {
+			this.userLogin()
+		}
+	},
+	onShow() {
+		utils.setTabbar(0, false)
+	},
+	methods: {
+		userLogin() {
+			uni.login({
+				success: res => {
+					if (res.code) {
+						login({
+							code: res.code,
+							platform_id: PLATFORM_ID
+						}).then((result) => {
+							console.log(result)
+							utils.cache(USER_DATA_KEY, result.data.token, 'd1')
+							utils.cache(USER_DATA_KEY_ID, result.data.id, 'd1')
+							this.title = result.data.token
+						}).catch((result) => {
+							console.error(result)
+							uni.showToast({
+								title: '请求登录失败',
+								icon: 'none',
+								duration: 3000
+							});
+						})
 					}
-				})
-			}
-		},
-	}
+				}
+			})
+		}
+	},
+}
 </script>
 
 <style>
-	page {
-		background: #ffffff;
-	}
+page {
+	background: linear-gradient(180deg, #60A5FA 0%, #C084FC 100%);
+}
 
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-	
-	.version{
-		color: #cccccc;
-		font-size: 10px;
-	  text-align: center;
-		margin-top: 25px;
-	}
-	
+.app-header{
+	height: 200rpx;
+}
+
+.content {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
+
+.version{
+	color: #ffffff;
+	font-size: 10px;
+	text-align: center;
+	margin-top: 25px;
+}
 </style>
